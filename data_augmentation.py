85f08e24a99df8b61336fd592d9653428e98c033
492235584 <492235584@qq.com>
2018-08-14 15:16:53 +0800

    modify indent and add l-r-u-p

--- data_augmentation.py
+++ data_augmentation.py
@@ -14,7 +14,7 @@ def randomcrop(filename, result_dir, suffix):
     crop_img = crop_img.eval()
     # 保存裁剪后的图片
     cv2.imwrite(result_dir + os.path.splitext(filename)[0] + '_' + str(suffix) + '.jpg',
-                crop_img)
+	        crop_img)
 
 # 图片翻转，上下，左右，对角
 def flip(filename, result_dir):
@@ -24,30 +24,36 @@ def flip(filename, result_dir):
     l_r_img = l_r_img.eval()
     # 保存左右翻转后的图片
     cv2.imwrite(result_dir + os.path.splitext(filename)[0] + '_' + '_l-r.jpg',
-                l_r_img)
-        
-                # 将图片进行上下翻转
-                l_r_img = tf.image.flip_up_down(img)
-                l_r_img = l_r_img.eval()
-                # 保存上下翻转后的图片
-                cv2.imwrite(result_dir + os.path.splitext(filename)[0] + '_' + '_u-d.jpg',
-                            l_r_img)
+	        l_r_img)
+
+    # 将图片进行上下翻转
+    u_d_img = tf.image.flip_up_down(img)
+    u_d_img = u_d_img.eval()
+    # 保存上下翻转后的图片
+    cv2.imwrite(result_dir + os.path.splitext(filename)[0] + '_' + '_u-d.jpg',
+                u_d_img)
+
+    # 将图片进行先左右再上下翻转
+    l_r_u_d_img = tf.image.flip_up_down(l_r_img)
+    l_r_u_d_img = l_r_u_d_img.eval()
+    # 保存上下翻转后的图片
+    cv2.imwrite(result_dir + os.path.splitext(filename)[0] + '_' + 'l_r_u-d.jpg',
+                l_r_u_d_img)
 
 NUM = 3
 if __name__ == "__main__":
-    result_dir = 'all_result/result/'
+    result_dir = 'all_result/result-flip/'
     if not os.path.exists(result_dir):
         os.mkdir(result_dir)
-    
+
     # with tf.Session() as sess:
     #     for filename in os.listdir(read_dir):
     #         for suffix in range(1,NUM + 1):
     #             randomcrop(filename, result_dir, suffix)
     #             print(filename + ' -------OK!!!')
-    
+
     with tf.Session() as sess:
         for filename in os.listdir(read_dir):
             flip(filename, result_dir)
             print(filename + ' -------OK!!!')
     sess.close()
-
